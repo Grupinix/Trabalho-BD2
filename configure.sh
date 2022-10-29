3f40314dc40c97cb773ce75f8cd62dca72cb865f
Yuri Nogueira <yurinogueira@id.uff.br>
2022-10-29 15:29:17 -0300

    use chinook

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker-compose exec oracle-xe sh -c "sqlplus / AS SYSDBA << EOF
 ALTER SESSION SET \"_ORACLE_SCRIPT\"=true;
-CREATE USER oracle_user IDENTIFIED BY oracle_password;
-GRANT CONNECT, RESOURCE, DBA TO oracle_user;
-GRANT ALL PRIVILEGES TO oracle_user;
+CREATE USER chinook IDENTIFIED BY p4ssw0rd;
+GRANT CONNECT, RESOURCE, DBA TO chinook;
+GRANT ALL PRIVILEGES TO chinook;
 EOF"
