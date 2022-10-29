0261811b95691cc45e9c000234211ef1f8879e29
Yuri Nogueira <yurinogueira@id.uff.br>
2022-10-29 15:12:26 -0300

    ajust configure.sh

--- configure.sh
+++ configure.sh
@@ -6,4 +6,4 @@ GRANT CONNECT, RESOURCE, DBA TO oracle_user;
 GRANT ALL PRIVILEGES TO oracle_user;
 EOF"
 
-docker-compose exec -i trabalho-bd2-oracle sqlplus / -u oracle_user -p oracle_password < /path/to/script.sql
+docker-compose exec -i trabalho-bd2-oracle sqlplus / -u oracle_user -p oracle_password < ./chinook_oracle.sql
