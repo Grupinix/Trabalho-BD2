5093ca591b937a4ce9c8b33485b276946b69ecf5
Yuri Nogueira <yurinogueira@id.uff.br>
2022-10-29 15:19:35 -0300

    adjust configure.sh

--- configure.sh
+++ configure.sh
@@ -5,5 +5,3 @@ CREATE USER oracle_user IDENTIFIED BY oracle_password;
 GRANT CONNECT, RESOURCE, DBA TO oracle_user;
 GRANT ALL PRIVILEGES TO oracle_user;
 EOF"
-
-docker-compose exec -i oracle-xe sqlplus / -u oracle_user -p oracle_password < ./chinook_oracle.sql
