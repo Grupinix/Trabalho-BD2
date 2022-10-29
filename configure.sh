478a97e203880585b693fc7067fd30d0e494473b
Yuri Nogueira <yurinogueira@id.uff.br>
2022-10-29 15:14:44 -0300

    fix configure.sh

--- configure.sh
+++ configure.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker-compose exec trabalho-bd2-oracle sh -c "sqlplus / AS SYSDBA << EOF
+docker-compose exec oracle-xe sh -c "sqlplus / AS SYSDBA << EOF
 ALTER SESSION SET \"_ORACLE_SCRIPT\"=true;
 CREATE USER oracle_user IDENTIFIED BY oracle_password;
 GRANT CONNECT, RESOURCE, DBA TO oracle_user;
 GRANT ALL PRIVILEGES TO oracle_user;
 EOF"
 
-docker-compose exec -i trabalho-bd2-oracle sqlplus / -u oracle_user -p oracle_password < ./chinook_oracle.sql
+docker-compose exec -i oracle-xe sqlplus / -u oracle_user -p oracle_password < ./chinook_oracle.sql
