7113c71f43286751b83f9fe17945afdfea8e7d52
Yuri Nogueira Moreira da Silva <yurinogueira@id.uff.br>
2022-11-10 10:14:31 -0300

    fix: questao3

--- Parte-2/3.sql
+++ Parte-2/3.sql
@@ -3,7 +3,7 @@
 
 
 
-CREATE OR REPLACE PROCEDURE INSERT_CUSTOMER(
+CREATE OR REPLACE PROCEDURE CHINOOK.INSERT_CUSTOMER(
     CUSTOMERID NUMBER,
     FIRSTNAME IN VARCHAR2,
     LASTNAME IN VARCHAR2,
@@ -22,19 +22,19 @@ BEGIN
     IF ((NOT COUNTRY LIKE 'B%') AND (NOT COUNTRY LIKE 'b%') AND (FIRSTNAME LIKE 'M%' OR FIRSTNAME LIKE 'm%')) THEN
         RAISE_APPLICATION_ERROR(-2025, 'NAÇÃO INVÁLIDA');
     END IF;
-    INSERT INTO CUSTOMER(CUSTOMERID,
-                         FIRSTNAME,
-                         LASTNAME,
-                         COMPANY,
-                         ADDRESS,
-                         CITY,
-                         STATE,
-                         COUNTRY,
-                         POSTALCODE,
-                         PHONE,
-                         FAX,
-                         EMAIL,
-                         SUPPORTREPID)
+    INSERT INTO CHINOOK.CUSTOMER(CUSTOMERID,
+                                 FIRSTNAME,
+                                 LASTNAME,
+                                 COMPANY,
+                                 ADDRESS,
+                                 CITY,
+                                 STATE,
+                                 COUNTRY,
+                                 POSTALCODE,
+                                 PHONE,
+                                 FAX,
+                                 EMAIL,
+                                 SUPPORTREPID)
     VALUES (CUSTOMERID,
             FIRSTNAME,
             LASTNAME,
@@ -53,27 +53,27 @@ END;
 /* Creating a USER without permission to Insert, only by Procedure */
 CREATE USER questao3 IDENTIFIED BY p4ssw0rd;
 GRANT CONNECT, RESOURCE, CREATE SESSION, ALTER SESSION TO questao3;
-GRANT SELECT ANY TABLE TO questao;
+GRANT SELECT ANY TABLE TO questao3;
 GRANT EXECUTE ON CHINOOK.INSERT_CUSTOMER TO questao3;
 
 
 /* Para testar use o novo usuário para se conectar e tente executar
    o insert ira dar erro, depois tente executar a procedure */
 
-INSERT INTO CUSTOMER(CUSTOMERID,
-                     FIRSTNAME,
-                     LASTNAME,
-                     COMPANY,
-                     ADDRESS,
-                     CITY,
-                     STATE,
-                     COUNTRY,
-                     POSTALCODE,
-                     PHONE,
-                     FAX,
-                     EMAIL,
-                     SUPPORTREPID)
-VALUES (87287,
+INSERT INTO CHINOOK.CUSTOMER(CUSTOMERID,
+                             FIRSTNAME,
+                             LASTNAME,
+                             COMPANY,
+                             ADDRESS,
+                             CITY,
+                             STATE,
+                             COUNTRY,
+                             POSTALCODE,
+                             PHONE,
+                             FAX,
+                             EMAIL,
+                             SUPPORTREPID)
+VALUES (87297,
         'MARC RUFALO',
         'Dubois',
         NULL,
@@ -86,4 +86,23 @@ VALUES (87287,
         NULL,
         'marc.dubois@hotmail.com',
         5
+);
+
+/* O insert não funcionou, alertando erro de privilégios insuficientes
+   agora iremos executar a procedure */
+
+CALL CHINOOK.INSERT_CUSTOMER(
+    87247,
+    'MARC RUFALO',
+    'Dubois',
+    NULL,
+    '11, Place Bellecour',
+    'Lyon',
+    NULL,
+    'Brasil',
+    69002,
+    '+33 04 78 30 30 30',
+    NULL,
+    'marc.dubois@hotmail.com',
+    5
 );
