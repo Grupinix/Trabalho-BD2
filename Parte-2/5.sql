fd045512aabfe86f79352556362fdcb4ce9b4700
mozart <thiagogarciamozart@gmail.com>
2022-11-23 18:08:25 -0300

    Feature: question 5 but still unfinished

--- Parte-2/5.sql
+++ Parte-2/5.sql
@@ -1,8 +1,20 @@
-CURSOR solucao
-IS
-SELECT INVOICEID
-FROM INVOICE
-WHERE
+/* Altera a tabela CUSTOMER, e cria uma coluna para
+   os números de sequência */
+
+ALTER TABLE CUSTOMER
+ADD sequence_number NUMBER;
+
+/* Faz um SELECT FOR UPDATE dessa nova coluna e
+   depois faz um UPDATE incrementando o valor da coluna */
+
+BEGIN
+    SELECT sequence_number
+    FROM CUSTOMER
     FOR UPDATE;
 
--- não sei o que fazer fml
+    UPDATE CUSTOMER C
+    SET C.sequence_number = C.sequence_number + 1;
+
+    COMMIT;
+end;
+
