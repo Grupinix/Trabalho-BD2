57ed1f984e5d84cb058fb608cc81ce7c60c7394e
Rodrigo Barroso <digorbarroso@gmail.com>
2022-11-23 21:53:25 -0300

    finishes part 2 question 5

--- Parte-2/5.sql
+++ Parte-2/5.sql
@@ -7,14 +7,25 @@ ADD sequence_number NUMBER;
 /* Faz um SELECT FOR UPDATE dessa nova coluna e
    depois faz um UPDATE incrementando o valor da coluna */
 
+CREATE OR REPLACE PROCEDURE seq
+    (customer_id NUMBER, firstname_n IN VARCHAR2, lastname_n IN VARCHAR2)
+AS
+actual_number NUMBER;
 BEGIN
-    SELECT sequence_number
-    FROM CUSTOMER
+    SET TRANSACTION READ WRITE;
+    SELECT C.sequence_number INTO actual_number
+    FROM CUSTOMER C WHERE C.CUSTOMERID = customer_id
     FOR UPDATE;
 
+
     UPDATE CUSTOMER C
-    SET C.sequence_number = C.sequence_number + 1;
+    SET C.sequence_number = (CASE WHEN actual_number IS NULL THEN 1 ELSE actual_number + 1 END),
+        C.FIRSTNAME = firstname_n, C.LASTNAME = lastname_n
+    WHERE C.CUSTOMERID = customer_id;
 
     COMMIT;
 end;
 
+CALL seq(1, 'teste', 'testado');
+
+SELECT * FROM CUSTOMER WHERE CUSTOMERID = 1;
